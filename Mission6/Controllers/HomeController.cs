47a08b8ef32838f2526d4f6bb0a05906b82ed76a
Gaberjames10 <gaberjames10@gmail.com>
2023-03-22 13:39:44 -0600

    Add project files.

---
+++ Mission6/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private MovieContext movieContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieContext x)
+        {
+            _logger = logger;
+            movieContext = x;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View("My Podcasts");
+        }
+
+        [HttpGet]
+        public IActionResult NewMovies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewMovies(MovieResponse mr)
+        {
+            movieContext.Add(mr);
+            movieContext.SaveChanges();
+            return View("Confirmation", mr);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission6/Models/MovieContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options) : base (options)
+        { }
+        public DbSet<MovieResponse> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieResponse>().HasData(
+
+                new MovieResponse
+                {
+                    MovieID = 1,
+                    Category = "Action/Sport",
+                    Title = "Creed II",
+                    Year = 2018,
+                    Director = "Steven Caple Jr.",
+                    Rating = "PG-13",
+
+                },
+
+                new MovieResponse
+                {
+                    MovieID = 2,
+                    Category = "Drama",
+                    Title = "Good Will Hunting",
+                    Year = 1997,
+                    Director = "Gus Van Sant",
+                    Rating = "R",
+
+                },
+
+                new MovieResponse
+                {
+                    MovieID = 3,
+                    Category = "Action",
+                    Title = "Ford v Ferrari",
+                    Year = 2019,
+                    Director = "James Mangold",
+                    Rating = "PG-13",
+
+                }
+                ) ;
+        }
+    }
+}
